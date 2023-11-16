6ee7f425baa6bdbf6a0db02a41225130a7926fdf
ornenovino <istar.novino@gmail.com>
2023-11-16 03:50:07 -0300

    updeteando footer

--- frontend/src/Components/layout/footer/Footer.jsx
+++ frontend/src/Components/layout/footer/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
     return (
         <>
             <Outlet />
-            <footer className="bg-[#202A44] text-white sm:h-20 md:h-20">
+            <footer className="bg-[#202A44] text-white sm:h-20 md:w-full p-0">
                 <div className="flex flex-col items-center justify-center lg:flex-row lg:justify-between">
-                    <div className="scroll-p-0 m-0 lg:mb-0">
+                    <div className="scroll-p-0 m-0 md:mb-0">
                         <img className="w-24" src="TOURuguaySinFondo.png" alt="Logo" />
                     </div>
                     <h5 className="text-center lg:text-end text-sm lg:ml-4">
