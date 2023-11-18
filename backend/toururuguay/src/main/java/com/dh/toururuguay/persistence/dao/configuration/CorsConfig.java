92074cfeb371fe234f7a618c95adf819f042a006
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-11-17 21:39:30 -0300

    correxion de CORS 3

--- backend/toururuguay/src/main/java/com/dh/toururuguay/persistence/dao/configuration/CorsConfig.java
+++ backend/toururuguay/src/main/java/com/dh/toururuguay/persistence/dao/configuration/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
