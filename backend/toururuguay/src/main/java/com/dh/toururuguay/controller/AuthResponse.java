41fd8f27df954f898f05a81c5cc0839cea463fec
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-11-22 19:57:42 -0300

    agregado del rol en AuthResponse para manejarlo en la ruta protegida

--- backend/toururuguay/src/main/java/com/dh/toururuguay/controller/AuthResponse.java
+++ backend/toururuguay/src/main/java/com/dh/toururuguay/controller/AuthResponse.java
@@ -13,12 +13,14 @@ public class AuthResponse {
     String token;
     private String name;
     private String lastname;
+    private String rol;
 
-    public static AuthResponse forLogin(String token, String name, String lastname) {
+    public static AuthResponse forLogin(String token, String name, String lastname, String rol) {
         return AuthResponse.builder()
                 .token(token)
                 .name(name)
                 .lastname(lastname)
+                .rol(rol)
                 .build();
     }
 }
