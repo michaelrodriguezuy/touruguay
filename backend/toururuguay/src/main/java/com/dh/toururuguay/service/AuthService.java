425baaea0a34eedf7e5f5c540151faa8a4cd3867
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-11-26 02:59:03 -0300

    correcion del AuthResponse

--- backend/toururuguay/src/main/java/com/dh/toururuguay/service/AuthService.java
+++ backend/toururuguay/src/main/java/com/dh/toururuguay/service/AuthService.java
@@ -33,16 +33,18 @@ public class AuthService {
 
             String name = "";
             String lastname = "";
+            String rol = "";
 
             if (user instanceof Usuario) {
                 Usuario usuario = (Usuario) user;
                 name = usuario.getName();
                 lastname = usuario.getLastname();
+                rol = usuario.getRol().getName();
             }
 
             // lo modifique para que la respuesta de login me devuelva el nombre y apellido
             // del usuario.
-            return AuthResponse.forLogin(token, name, lastname);
+            return AuthResponse.forLogin(token, name, lastname, rol);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
