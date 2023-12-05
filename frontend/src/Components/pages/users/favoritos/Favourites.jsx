a23e546f80a7148d358147bc05cd6966939bc2b2
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-12-05 17:52:45 -0300

    favoritos

--- frontend/src/Components/pages/users/favoritos/Favourites.jsx
+++ frontend/src/Components/pages/users/favoritos/Favourites.jsx
@@ -3,7 +3,7 @@ import { DataContext } from "../../../context/DataContext";
 import ProductCard from "../../../layout/cards/ProductCard";
 
 const Favoritos = () => {
-  const { fetchFavourites, favourites } = useContext(DataContext);
+  const { fetchFavourites, favourites, products } = useContext(DataContext);
   const [isLiked, setIsLiked] = useState(false);
 
   useEffect(() => {
@@ -21,15 +21,19 @@ const Favoritos = () => {
       <h2 className="text-center text-2xl p-10 font-bold">Mis favoritos</h2>
 
       <div className="grid grid-cols-1 gap-4 width-full md:grid-cols-2 md:max-w-4xl m-auto">
-        {favourites && favourites.length>0 ? (
-          favourites.map((favorite) => (
+        {favourites && favourites.length > 0 ? (
+          favourites.map((favorite) => {
+            const product = products.find(
+              (product) => product.product_id === favorite.product_id
+            );
+            
             <ProductCard
-              key={favorite.favorite_id}
-              product={favorite.product}
+              key={product.product_id}
+              product={product}
               setIsLiked={setIsLiked}
               isLiked={isLiked}
-            />
-          ))
+            />;
+          })
         ) : (
           <p>No tienes favoritos</p>
         )}
