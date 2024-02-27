812285c9bb9a1232d910d1751fc113d8ae08f01f
Michael Rodríguez <hola@olimarteam.uy>
2024-02-27 11:26:56 -0300

    Update Galeria.jsx

--- frontend/src/Components/pages/products/Galeria.jsx
+++ frontend/src/Components/pages/products/Galeria.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { DataContext } from "src/Components/context/dataContext";
+import { DataContext } from "/src/Components/context/dataContext";
 
 const Galeria = () => {
   const { productId } = useParams();
